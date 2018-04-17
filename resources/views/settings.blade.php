a4aa38f86b344ecd59391963e84e4774e18e4fd0
Ryosuke <ryosuke.san.hana@gmail.com>
2018-04-16 18:00:05 -0700

    Hard coding sqlite DB

---
+++ resources/views/settings.blade.php
@@ -0,0 +1,13 @@
+ @extends('layouts.app')
+
+    @section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">            
+                <div id="clients"></div>
+                <div id="AuthorizedClients"></div>
+                <div id="PersonalAccessTokens"></div>
+            </div>
+        </div>
+    </div>
+    @endsection
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware(['auth'])->group(function () {
+    Route::view('/settings', 'settings')->name('settings');
+});
